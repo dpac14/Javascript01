9be1a58d6ce819cdc8d54739252cf7f8058eb05a
dpac14 <dipakchute11@gmail.com>
2025-03-10 21:35:43 +0530

    added new

--- 01Js_basics/10-object.js
+++ 01Js_basics/10-object.js
@@ -6,10 +6,10 @@ const mySym = Symbol("key1")
 
 const JsUser = {
     name: "Dpak14",
-    "full name" : "Dipak Chute",
+    "full name": "Dipak Chute",
     [mySym]: "mykey1",
     age: 22,
-    location: "Nagpur",
+    location: "Nagpur ",
     email: "dpakchute11@gmail.com",
     isLoggedIn: "false",
 }
@@ -18,24 +18,24 @@ const JsUser = {
 // console.log(JsUser ["full name"]);
 // console.log(JsUser [mySym]);
 
- JsUser.email = "dipakchute@googel.com"
+JsUser.email = "dipakchute@googel.com"
 //  console.log(JsUser);
 //  console.log(JsUser.email);
- 
+
 // //  Object.freeze(JsUser)
 //  JsUser.email ="dipak@chatgpt"
 
- JsUser.greeting= function(){
+JsUser.greeting = function () {
     console.log("Hellow Javascript User");
- } 
+}
 
-JsUser.greetingTwo= function(){
+JsUser.greetingTwo = function () {
     console.log(`Hellow JS User, ${this.name}`);
- }
+}
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
 
 
- 
+
 
 
