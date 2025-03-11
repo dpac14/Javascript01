52d8a9b9966e65722c21c9ef46d5a22588ae9d5a
dpac14 <dipakchute11@gmail.com>
2025-03-11 22:03:05 +0530

    added new line

--- 02Js_basics/01-function.js
+++ 02Js_basics/01-function.js
@@ -4,18 +4,19 @@ function sayMyName() {
   console.log("I");
   console.log("P");
   console.log("A");
-  console.log("C");
+  console.log("K");
 
 }
 // sayMyName()
 
 
-// function addTwoNumbers(number1, number2){
+function addTwoNumbers(number1, number2){
 
-//   console.log(number1 + number2);
+  console.log(number1 + number2);
 
-// }
-// // addTwoNumbers(3, "4")
+}
+addTwoNumbers(3, "4") 
+
 
 
 function addTwoNumbers(number1, number2) {
@@ -42,30 +43,30 @@ function loginUserMessage(username = "Uesre Not ") {
 // console.log(loginUserMessage());
 
 
-function calculateCartPrice (val1, val2, ...num1){
-return num1
+function calculateCartPrice(val1, val2, ...num1) {
+  return num1
 }
 // console.log(calculateCartPrice(200, 400, 500, 2000));
 
-const uesr ={
+const uesr = {
   username: "Dipak",
-  price : 199
+  price: 199
 }
-function handleObject(anyObject){
+function handleObject(anyObject) {
   console.log(`Usernmae is ${anyObject.username} and Price is ${anyObject.price}`)
-  
+
 }
 
-  // handleObject(uesr)
-  handleObject({
-    username: "sam ",
-    price:399
-  })
+// handleObject(uesr)
+handleObject({
+  username: "sam ",
+  price: 399
+})
 
-  const myNewArray =[ 200, 300, 400,]
+const myNewArray = [200, 300, 400,]
 
-  function returnSecondValue(getArray){
-    return getArray[1]
-  }
-  // console.log(returnSecondValue(myNewArray))
-  console.log(returnSecondValue([200, 300, 400]))
+function returnSecondValue(getArray) {
+  return getArray[1]
+}
+// console.log(returnSecondValue(myNewArray))
+// console.log(returnSecondValue([200, 300, 400]))
