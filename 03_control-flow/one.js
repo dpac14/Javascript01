249fb2d59e0ac1b043daa8fa001339590a17f3b9
dpac14 <dipakchute11@gmail.com>
2025-03-19 12:20:09 +0530

    adde some line

--- 03_control-flow/one.js
+++ 03_control-flow/one.js
@@ -1,8 +1,25 @@
 // if 
 
-const isuserloggedin = true
-
-if ( 2 === "2") {
-    console.log("executed");
+const isUserloggedin = true
+const temperature = 42 
+// if ( temperature === 40) {
+//     console.log("less than 50");
     
-    }
+//     } else{ 
+//         console.log("temperature is greater than 50");
+
+//     }
+
+
+
+      const score = 200
+      if(score > 100) {
+        let power ="fly"
+        console.log(`User power : ${power}`);
+      
+        
+      }
+        //   console.log(`User power : ${power}`);
+        
+    
+
