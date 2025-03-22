b8321c0cffa9dbe00cf558cfd5110aaddbd6a43a
dpac14 <dipakchute11@gmail.com>
2025-03-22 11:36:44 +0530

    fix truthy-falsy values

--- 03_control-flow/truthy-falsy.js
+++ 03_control-flow/truthy-falsy.js
@@ -1,13 +1,13 @@
 const userEmail = []
- if (userEmail) {
-    console.log("Got user email");
+if (userEmail) {
+   console.log("Got user email");
 
- } else{
-    console.log("Don't have user email");
-    
- }
+} else {
+   console.log("Don't have user email");
 
- // Falsy  Values 
+}
+
+// Falsy  Values 
 //  false, 0 , -0, BigInt, 0n, "", null, undefined, NaN
 
 //Truthy Values
@@ -16,26 +16,28 @@ const userEmail = []
 // if (userEmail.length === 0) {
 //    console.log("Array is empty");
 // }
- const emtyObj ={}
+
+const emtyObj = {}
 if (Object.keys(emtyObj).length === 0) {
-   console.log("Object is empty");
-   
+   // console.log("Object is empty");
+
 }
 
 // Nullish Coalescing Operator (??): null undefined
- let val1;
-//  val1 = 5 ?? 10
+let val1;
+val1 = 5 ?? 10
 //  val1 = null ?? 10
 //  val1 = undefined ?? 16
 //  val1 = null ?? 10 ?? 20
 
-//  console.log(val1);
+console.log(val1);
 
 
 //  Terniary Operator
+
 // condition ? true : false
 
-const iceTeaPirce = 100 
+const iceTeaPirce = 100
 iceTeaPirce >= 80 ? console.log("Less Than 80") : console.log("more than 80 ");
 
 
