ef3e9e144d709b8ba7e6970a9447de891b72784b
dpac14 <dipakchute11@gmail.com>
2025-03-25 13:57:06 +0530

    added new file

---
+++ 04_iterations/five.js
@@ -0,0 +1,39 @@
+const coding =  ["js", "ruby", "java", "python", "cpp"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+    
+// })
+// coding.forEach(  (item) => {
+//     console.log(item);
+    
+// })
+
+// function printMe(item) {
+//     console.log(item);
+// }
+// coding.forEach(printMe)
+
+// coding.forEach( (index, item, arr)=> {
+//     console.log(index, item, arr);
+// })
+
+const myCoding = [
+    {
+       languageName: "Javascript",
+       languageFilename: "JS"
+    },
+    {
+       languageName: "Java",
+       languageFilename: "java"
+    },
+    {
+       languageName: "Python",
+       languageFilename: "py"
+    },
+]
+myCoding.forEach((item) => {
+    console.log(item.languageFilename);
+    
+
+})
