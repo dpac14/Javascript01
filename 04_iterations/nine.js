4c1452d126157261c318cdff47dac2157ca3a5c9
dpac14 <dipakchute11@gmail.com>
2025-03-31 14:29:59 +0530

    added new file for Reduce

--- 04_iterations/nine.js
+++ 04_iterations/nine.js
@@ -1,12 +1,35 @@
 //  Reduce
- const myNums =[1, 2, 3]
+const myNums = [1, 2, 3]
 
 
 // const myTotal = myNums.reduce(function (acc, currval){
 //     // console.log(`acc: ${acc} and currval: ${currval}`);
 //     return acc + currval
-    
+
 // }, 0)
 
 const myTotal = myNums.reduce((acc, currval) => acc + currval, 0)
-console.log(myTotal);
+
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 399
+    },
+    {
+        itemName: "Py course",
+        price: 999
+    },
+    {
+        itemName: "data science course",
+        price: 15999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+
+]
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+console.log(priceToPay);
