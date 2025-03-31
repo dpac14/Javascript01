f0cdc762dc57c9457b3366abbefeb0b3af80ebd3
dpac14 <dipakchute11@gmail.com>
2025-03-31 13:08:09 +0530

    added new line

--- 04_iterations/six.js
+++ 04_iterations/six.js
@@ -1,30 +1,30 @@
 // const coding = ["js", "ruby", "java", "python", "cpp"]
 
-// const values = coding.forEach((item) =>{
-//     console.log(item);
-//     return item
-    
-// })
+const values = coding.forEach((item) => {
+  console.log(item);
+  return item
+
+})
 // console.log(values);
 
 
-const myNums =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNum = myNums.filter((num)=> {
-//      return num > 4
-// })
+// const newNum = myNums.filter((num) => {
+  //      return num > 4
+  // })
 
-// const newNum =[]
-// myNums.forEach ( (num)=>{
-//     if (num > 4) {
-//         newNum.push(num)
-        
-//     }
-// })
-// console.log(newNum);
+  const newNum = []
+  myNums.forEach((num) => {
+    if (num > 4) {
+      newNum.push(num)
+
+          }
+    })
+  // console.log(newNum);
 
 
-const books = [
+  const books = [
     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
     { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
     { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
@@ -36,9 +36,9 @@ const books = [
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
   ];
 
-  let userBooks = books.filter( (bk) => bk.genre === 'History')
-  
-  userBooks = books.filter((bk) =>{ 
+  let userBooks = books.filter((bk) => bk.genre === 'History')
+
+  userBooks = books.filter((bk) => {
     return bk.publish >= 1995 && bk.genre === 'History'
-} )
+  })
   console.log(userBooks);
