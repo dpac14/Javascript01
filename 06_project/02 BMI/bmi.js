9153949e564244127b3463d3f25653fe8b7fcfa8
dpac14 <dipakchute11@gmail.com>
2025-05-20 12:41:27 +0530

    addded new line is JS file

--- 06_project/02 BMI/bmi.js
+++ 06_project/02 BMI/bmi.js
@@ -1,7 +1,34 @@
-const  form = document.querySelector('form')
+const form = document.querySelector('form')
+//this usecase will give you empty value
+// const height = parseInt(document.querySelector('#height').value)
 
-form.addEventListener('submit', function (e){
-e.preventDefault()
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
 
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const results = document.querySelector('#results')
+
+    if (height === '' || height < 1 || isNaN(height)) {
+        results.innerHTML = `please give a valid height ${height}`
+    } else if (weight === '' || weight < 1 || isNaN(weight)) {
+        results.innerHTML = `please give a valid weight ${weight}`
+    } else {
+        const bmi = (weight / ((height * weight) / 10000)).toFixed(1)
+        //show the result
+        if (bmi < 18.6) {
+            results.innerHTML =`<spam>${bmi}</spam><spam>Under Weight = Less than 18.6</spam>`
+            
+        }
+        else if (bmi < 18.5 && bmi < 25 ) {
+            results.innerHTML =`<spam>${bmi}</spam><spam>Range = 18.6 and 24.9</spam>`
+            
+        }
+        else if (bmi > 24.9 ) {
+            results.innerHTML =`<spam>${bmi}</spam><spam>Greater than 24.9</spam>`
+            
+        }
+        
+    }
 
 })
