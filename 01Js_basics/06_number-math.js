73c39ea64fe1c4d7f3b27036aacdd7e961c09103
dpac14 <dipakchute11@gmail.com>
2025-08-12 14:33:01 +0530

    some changes

--- 01Js_basics/06_number-math.js
+++ 01Js_basics/06_number-math.js
@@ -1,26 +1,26 @@
 const scroe = 400
 // console.log(scroe);
- const balance = new Number(100)
+const balance = new Number(100)
 //  console.log(balance.toString());
- 
 
 
-//  console.log(balance.toString().length);
-//  console.log(balance.toFixed(1));
- 
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
 
 
- const otherNumber =123.8047
-//   console.log(otherNumber.toPrecision(4));
 
-  const hundreds = 100000
-//   console.log(hundreds.toLocaleString(`en-IN`));
-  
-  
+const otherNumber = 123.8047
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString(`en-IN`));
+
+
 //++++++++++++++++Maths++++++++++++++++++
 
-console.log(Math);
-// console.log(Math.abs(-4));
+// console.log(Math);
+// console.log(Math.abs(-4)); 
 // console.log(Math.round(4.6));
 // console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9));
